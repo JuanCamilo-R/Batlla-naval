d2958ab559a5d051c62cd14aa18e89165c827b6c
JuanCamilo-R <51805884+JuanCamilo-R@users.noreply.github.com>
2020-09-20 18:47:26 -0500

    Se implementa funcion agregarBarcos

--- src/batallaNaval/PantallaUsuario.java
+++ src/batallaNaval/PantallaUsuario.java
@@ -4,21 +4,73 @@ import java.util.ArrayList;
 import java.awt.Point;
 
 public class PantallaUsuario {
-	//Atributos
-	private Barcos [][] barcosPantalla = new Barcos[10][10];
+//Atributos
+private Barcos [][] barcosPantalla = new Barcos[10][10];
+private int [][] posicionesBarco = new int[10][10];
+private ArrayList<Point> posicionesBarcoActual = new ArrayList<Point>();
+
 	//Metodos
 	public PantallaUsuario() {
 		
 	}
-	public void estaDisponible() {
+	public boolean estaDisponible(int x1,int y1,int x2, int y2,int tamano) {
+		if(barcosPantalla[x1][y1]==null && barcosPantalla[x2][y2]==null) {
+			if(tamano > 2 && verificarMitad( x1,y1,x2,y2, tamano)) {
+				return true;
+			}
+			return true;
+		}
 		
+		return false;
+		
+	}
+	private boolean verificarMitad(int x1,int y1,int x2, int y2,int tamano) {
+		
+		int distancia = (int) Math.sqrt(Math.pow(x2-x1,2)+Math.pow(y2-y1,2)) + 1;
+		
+		if( x1 == x2 && distancia == tamano) {
+			for(int i = 1; i <= (y2-y1-1); i++) {
+				if(barcosPantalla[x1][y1+i] != null) {
+					return false;
+				}
+			}
+			return true;
+		}
+		else if( y1 == y2 && distancia == tamano) {
+			for(int i = 1; i <= (x2-x1-1); i++) {
+				if(barcosPantalla[x1+i][y1] != null) {
+					return false;
+				}
+			}
+			return true;
+		}
+		
+		return false;
 	}
 	//1er numero > 2ndo numero //Izquierda
 	//1er numero < 2nd numero //Derecha
 	//[1,3] [1,5]  1 == 1, 3 < 5 //Derecha
 	//[1,3] [2,4] 1 == 2 ? , 3 == 4 ?
 	//[1,5] [1,3] 1 == 1, 5 > 3 //Izquierda
-	public void ponerBarco(ArrayList<Point> primerPar, ArrayList<Point> segundoPar) {
+	public void ponerBarco(int x1,int y1,int x2, int y2, int tamano, Barcos barco) {
+		
+		if(estaDisponible(x1,x2,y1,y2,tamano)) {
+			int contador = 0;
+			do {
+				if(x1 == x2) {
+					posicionesBarcoActual.add(new Point(x1,y1 + contador));
+					barcosPantalla[x1][y1+contador] = barco;
+				}
+				if(y1 == y2) {
+					posicionesBarcoActual.add(new Point(x1 + contador,y1));
+					barcosPantalla[x1+contador][y1] = barco;
+				}
+				contador++;
+			}while(contador != tamano);
+		}
+	}
+	
+public void llenarPosicion(int x1,int y1,int x2,int y2) {
 		
 	}
 }
