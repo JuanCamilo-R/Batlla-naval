f543e46390a4305cd31a09baf3b4ab56d1920811
jennyferbm13 <51867672+jennyferbm13@users.noreply.github.com>
2020-10-09 00:11:48 -0500

    javaDoc

--- src/batallaNaval/PantallaUsuario.java
+++ src/batallaNaval/PantallaUsuario.java
@@ -8,18 +8,38 @@ package batallaNaval;
 import java.util.ArrayList;
 import java.awt.Point;
 
+// TODO: Auto-generated Javadoc
+/**
+ * The Class PantallaUsuario.
+ */
 public class PantallaUsuario {
+	
+/** The barcos pantalla. */
 //Atributos
 private Barcos [][] barcosPantalla = new Barcos[10][10];
 
 	
+	/**
+	 * Instantiates a new pantalla usuario.
+	 */
 	//Metodos
 	public PantallaUsuario() {
 		
 	}
+	
+	/**
+	 * Esta disponible.
+	 *
+	 * @param x1 the x 1
+	 * @param y1 the y 1
+	 * @param x2 the x 2
+	 * @param y2 the y 2
+	 * @param tamano the tamano
+	 * @return true, if successful
+	 */
 	protected boolean estaDisponible(int x1,int y1,int x2, int y2,int tamano) {
 		int distancia = ((int) Math.sqrt(Math.pow(x2-x1,2)+Math.pow(y2-y1,2))) + 1;
-		if(barcosPantalla[x1][y1]==null && barcosPantalla[x2][y2]==null && tamano ==distancia) {
+		if(barcosPantalla[x1][y1]==null && barcosPantalla[x2][y2]==null && tamano ==distancia && (x1==x2 || y1==y2 )) {
 			if(tamano > 2) {
 				if(verificarMitad(x1, y1, x2, y2, tamano)) {
 					return true;
@@ -33,32 +53,58 @@ private Barcos [][] barcosPantalla = new Barcos[10][10];
 		
 	}
 	
+	/**
+	 * Hay barco.
+	 *
+	 * @param x the x
+	 * @param y the y
+	 * @return true, if successful
+	 */
 	public boolean hayBarco(int x, int y) {
 		if(barcosPantalla[x][y] != null) {
 			return true;
 		}
 		return false;
 	}
+	
+	/**
+	 * Barco vivo.
+	 *
+	 * @param x the x
+	 * @param y the y
+	 * @return true, if successful
+	 */
 	public boolean barcoVivo(int x, int y) {
 		return barcosPantalla[x][y].isVivo();
 	}
+	
+	/**
+	 * Verificar mitad.
+	 *
+	 * @param x1 the x 1
+	 * @param y1 the y 1
+	 * @param x2 the x 2
+	 * @param y2 the y 2
+	 * @param tamano the tamano
+	 * @return true, if successful
+	 */
 	protected boolean verificarMitad(int x1,int y1,int x2, int y2,int tamano) {
-		int direcci髇 = 1;
+		int direcci贸n = 1;
 		if(x1==x2) {
 			if(y2-y1<0) {
-				direcci髇=-1;
+				direcci贸n=-1;
 			}
 		}
 		if(y1==y2) {
 			if(x2-x1<0) {
-				direcci髇=-1;
+				direcci贸n=-1;
 			}
 		}
 		if( x1 == x2) {
 			System.out.println("Horizontal");
 			for(int i = 0; i <= Math.abs(y2-y1)-1; i++) {
-				System.out.println("Verificando posici髇 x: "+(x1)+",y: "+(y1+i*direcci髇));
-				if(barcosPantalla[x1][y1+i*direcci髇] != null) {
+				System.out.println("Verificando posici贸n x: "+(x1)+",y: "+(y1+i*direcci贸n));
+				if(barcosPantalla[x1][y1+i*direcci贸n] != null) {
 					System.out.println("Ocupado");
 					return false;
 			 	}
@@ -68,8 +114,8 @@ private Barcos [][] barcosPantalla = new Barcos[10][10];
 		else if( y1 == y2 ) {
 			System.out.println("Vertical");
 			for(int i = 1; i <= Math.abs(x2-x1)-1; i++) {
-				System.out.println("Verificando posici髇 x: "+(x1+i*direcci髇)+",y: "+(y1));
-				if(barcosPantalla[x1+i*direcci髇][y1] != null) {
+				System.out.println("Verificando posici贸n x: "+(x1+i*direcci贸n)+",y: "+(y1));
+				if(barcosPantalla[x1+i*direcci贸n][y1] != null) {
 					System.out.println("Ocupado");
 					return false;
 				}
@@ -80,6 +126,9 @@ private Barcos [][] barcosPantalla = new Barcos[10][10];
 		return false;
 	}
 	
+	/**
+	 * Mostrar.
+	 */
 	public void mostrar() {
 		for(int i = 0; i < 10; i++) {
 			for(int j = 0; j < 10; j++) {
@@ -96,45 +145,55 @@ private Barcos [][] barcosPantalla = new Barcos[10][10];
 	//1er numero < 2nd numero //Derecha
 	//[1,3] [1,5]  1 == 1, 3 < 5 //Derecha
 	//[1,3] [2,4] 1 == 2 ? , 3 == 4 ?
+	/**
+	 * Poner barco.
+	 *
+	 * @param x1 the x 1
+	 * @param y1 the y 1
+	 * @param x2 the x 2
+	 * @param y2 the y 2
+	 * @param tamano the tamano
+	 * @param barco the barco
+	 */
 	//[1,5] [1,3] 1 == 1, 5 > 3 //Izquierda
 	public void ponerBarco(int x1,int y1,int x2, int y2, int tamano, Barcos barco) {
 		if(estaDisponible(x1,y1,x2,y2,tamano)) {
 			int contador = 0;
-			int direcci髇 = 1;
+			int direcci贸n = 1;
 			if(x1==x2) {
 				if(y2-y1<0) {
-					System.out.println("Direcci髇 izquierda");
+					System.out.println("Direcci贸n izquierda");
 					barco.setDireccion("izquierda");
-					direcci髇=-1;
+					direcci贸n=-1;
 				}
 				else {
-					System.out.println("Direcci髇 derecha");
+					System.out.println("Direcci贸n derecha");
 					barco.setDireccion("derecha");
 				}
-				System.out.print("Despu閟 del else de derecha");
+				System.out.print("Despu茅s del else de derecha");
 			}
 			else if(y1==y2) {
 				if(x2-x1<0) {
-					System.out.println("Direcci髇 abajo");
+					System.out.println("Direcci贸n abajo");
 					barco.setDireccion("abajo");
-					direcci髇=-1;
+					direcci贸n=-1;
 				}
 				else {
-					System.out.println("Direcci髇 arriba");
+					System.out.println("Direcci贸n arriba");
 					barco.setDireccion("arriba");
 				}
 			}
 			System.out.println("Antes del do while");
 			do {
 				if(x1 == x2) {
-					barco.anadir(new Point(x1,y1+contador*direcci髇));
-					barcosPantalla[x1][y1+contador*direcci髇] = barco;
-					System.out.println("Puso el barco en x1: "+x1+",y1: "+(y1+contador*direcci髇));
+					barco.anadir(new Point(x1,y1+contador*direcci贸n));
+					barcosPantalla[x1][y1+contador*direcci贸n] = barco;
+					System.out.println("Puso el barco en x1: "+x1+",y1: "+(y1+contador*direcci贸n));
 				}
 				if(y1 == y2) {
-					barco.anadir(new Point(x1+contador*direcci髇,y1));
-					barcosPantalla[x1+contador*direcci髇][y1] = barco;
-					System.out.println("Puso el barco en x1: "+(x1+contador*direcci髇)+",y1: "+y1);
+					barco.anadir(new Point(x1+contador*direcci贸n,y1));
+					barcosPantalla[x1+contador*direcci贸n][y1] = barco;
+					System.out.println("Puso el barco en x1: "+(x1+contador*direcci贸n)+",y1: "+y1);
 				}
 				System.out.print("Puso el barco \n");
 				contador++;
@@ -147,6 +206,9 @@ private Barcos [][] barcosPantalla = new Barcos[10][10];
 		}
 	}
 	
+	/**
+	 * Limpiar pantalla.
+	 */
 	public void limpiarPantalla() {
 		for(int i = 0; i < 10; i++) {
 			for(int j = 0; j < 10; j++) {
@@ -154,4 +216,5 @@ private Barcos [][] barcosPantalla = new Barcos[10][10];
 			}
 		}
 	}
+	
 }
